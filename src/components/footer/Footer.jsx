f433c4987211befb5c40442207e138f5179dee11
Somya Kislay <somukislay@gmail.com>
2024-09-23 18:31:50 +0530

    Auto commit message

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,44 +1,38 @@
-import "./footer.css"
+import React from 'react';
+import './Footer.css';
 
-const Footer =() =>{
-    return(
-        <div className="footer">
-            <div className="fLists">
-                <ul className="fList">
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Regions</li>
-                    <li className="fListItem">Cities</li>
-                    <li className="fListItem">Districts</li>
-                    <li className="fListItem">Airport</li>
-                    <li className="fListItem">Hotels</li>
-
-                </ul>
-
-                <ul className="fList">
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Regions</li>
-                    <li className="fListItem">Cities</li>
-                    <li className="fListItem">Districts</li>
-                    <li className="fListItem">Airport</li>
-                    <li className="fListItem">Hotels</li>
-
-                </ul>
-
-                <ul className="fList">
-                    <li className="fListItem">Countries</li>
-                    <li className="fListItem">Regions</li>
-                    <li className="fListItem">Cities</li>
-                    <li className="fListItem">Districts</li>
-                    <li className="fListItem">Airport</li>
-                    <li className="fListItem">Hotels</li>
-
-                </ul>
-
-            </div>
-            <div className="fText">Copyright 2024</div>
-
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h3>About Flyingo</h3>
+          <p>Flyingo is your go-to platform for booking flights and exploring travel options with ease.</p>
         </div>
-    )
+        <div className="footer-section">
+          <h3>Quick Links</h3>
+          <ul>
+            <li><a href="/flights">Flights</a></li>
+            <li><a href="/hotels">Hotels</a></li>
+            <li><a href="/contact">Contact Us</a></li>
+            <li><a href="/about">About Us</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h3>Connect with Us</h3>
+          <ul className="social-links">
+            <li><a href="https://www.facebook.com">Facebook</a></li>
+            <li><a href="https://www.twitter.com">Twitter</a></li>
+            <li><a href="https://www.instagram.com">Instagram</a></li>
+            <li><a href="https://www.linkedin.com">LinkedIn</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>Copyright © Somya, 2024. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-}
-export default Footer
+export default Footer;
